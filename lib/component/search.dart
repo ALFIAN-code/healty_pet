b5d536ec28e59a06b145161c0409aed24ac51dee
BAGUS ALFIAN <allvvnt@gmail.com>
2023-07-05 19:38:13 +0700

    add search suggesion

--- lib/component/search.dart
+++ lib/component/search.dart
@@ -1,5 +1,6 @@
+import 'package:feather_icons/feather_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:healty_pet/style/style.dart';
 
 class Search extends StatelessWidget {
@@ -10,24 +11,23 @@ class Search extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
       decoration: BoxDecoration(
         color: Color.fromARGB(255, 240, 240, 240),
         borderRadius: BorderRadius.circular(15)
       ),
       child: Row(
         children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(22, 10, 5, 10),
-            child: SvgPicture.asset('lib/asset/image/search.svg', height: 25,),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(22, 10, 5, 10),
+            child: Icon(FeatherIcons.search, color: Colors.black54)
           ),
 
           Expanded(child: TextField(
-            style: bold16.copyWith(color: const Color.fromARGB(255, 105, 105, 105)),
+            style:GoogleFonts.manrope(textStyle: bold16.copyWith(color: const Color.fromARGB(255, 105, 105, 105))),
             decoration: InputDecoration(
             
               hintText: 'Find best vaccinate, treatment...',
-              hintStyle: bold16.copyWith(color: const Color.fromARGB(255, 196, 195, 195)),
+              hintStyle: GoogleFonts.manrope(textStyle: bold16.copyWith(color: const Color.fromARGB(255, 196, 195, 195))),
               border: const OutlineInputBorder(
                 borderSide: BorderSide.none
               )
